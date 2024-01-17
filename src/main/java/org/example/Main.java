93cd3d21df148afeeb22dbd5ba91358acf7bcb54
Pikolinian <42726269+pikolinianita@users.noreply.github.com>
2024-01-17 19:19:50 +0100

    Best wishes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Pull request test");
+        System.out.println("Best wishes to contributors and reviewers");
     }
 }
